d31a7889f91b5d2d1f157bd9b1f694d602aad113
logist784 <logist_blh@mail.ru>
2025-11-18 20:05:53 +0300

    Add_5_lesson

---
+++ lesson_05/lesson05_task4.2.py
@@ -0,0 +1,20 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Firefox()
+
+try:
+    driver.get("http://the-internet.herokuapp.com/login")
+    username_field = driver.find_element(By.ID, "username")
+    username_field.send_keys("tomsmith")
+    password_field = driver.find_element(By.ID, "password")
+    password_field.send_keys("SuperSecretPassword!")
+    login_button = driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    login_button.click()
+    time.sleep(2)
+    success_message = driver.find_element(By.ID, "flash")
+    print("Текст с зеленой плашки:", success_message.text)
+    time.sleep(2)
+finally:
+    driver.quit()
